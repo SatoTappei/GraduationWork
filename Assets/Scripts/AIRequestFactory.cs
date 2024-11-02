46cd519f1c9363b1a120f7f56ad2e8aef5105050
Tappei <stutpi.signup@gmail.com>
2024-11-02 19:45:40 +0900

    age

--- Assets/Scripts/AIRequestFactory.cs
+++ Assets/Scripts/AIRequestFactory.cs
@@ -6,7 +6,7 @@ namespace Game
 {
     public static class AIRequestFactory
     {
-        const string Key = "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";
+        const string Key = "";
 
         public static AIRequest Create(string content, int capacity = 7)
         {
