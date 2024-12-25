55733b371d1396b249a02bdd1223d23934d5ff3d
Tappei <stutpi.signup@gmail.com>
2024-12-25 14:02:31 +0900

    あげ

--- Assets/Scripts/DungeonEntity/EnemySpawner.cs
+++ Assets/Scripts/DungeonEntity/EnemySpawner.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Game
 {
     public class EnemySpawner : DungeonEntity
     {
-        [SerializeField] Enemy _prefab;
+        [System.Serializable]
+        class Data
+        {
+            [SerializeField] Enemy _prefab;
+            [SerializeField] int _weight;
+
+            public Enemy Prefab => _prefab;
+            public int Weight => _weight;
+        }
+
+        [SerializeField] Data[] _data;
 
         Enemy _spawned;
         WaitUntil _waitDefeated;
@@ -21,7 +32,7 @@ namespace Game
         {
             while (true)
             {
-                _spawned = Instantiate(_prefab);
+                _spawned = Choice();
                 _spawned.Initialize(Coords);
 
                 yield return _waitDefeated ??= new WaitUntil(IsDefeated);
@@ -29,6 +40,22 @@ namespace Game
                 // “G‚ªŒ‚”j‚³‚ê‚Ä‚©‚çÄ“x—N‚­‚Ü‚Å‚ÌŠÔŠuB“K“–‚ÉŽžŠÔ‚ðŽw’èB
                 yield return _waitInterval ??= new WaitForSeconds(10.0f);
             }
+        }
+
+        Enemy Choice()
+        {
+            int sum = _data.Select(d => d.Weight).Sum();
+            int random = Random.Range(0, sum) + 1;
+            foreach (Data data in _data)
+            {
+                int range = sum - data.Weight;
+                if (range < random) return data.Prefab;
+
+                sum = range;
+            }
+
+            // Šm—¦‚ÌÝ’è‚ÉˆÙí‚ª‚ ‚éê‡‚ÍAˆê”Ôo‚â‚·‚¢“G‚ð•Ô‚·B
+            return _data.OrderByDescending(d => d.Weight).FirstOrDefault().Prefab;
         }
 
         bool IsDefeated()
